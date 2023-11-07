62246cc7764eeabf104146939c0deaa3604860d8
aro04 <mandaaroniaina2@gmail.com>
2023-11-07 10:41:38 -0500

    MonPremierProjet

---
+++ Projet-1-Automne-2023-main/Projet-1-Automne-2023-main/FrameProjet-1/FrameProjet-1/Program.cs
@@ -0,0 +1,62 @@
+﻿string[,] tableau = new string[10, 7];
+startTheMachine();
+
+void afficherMenu()
+{
+
+}
+
+void startTheMachine()
+{
+    
+}
+
+void selectChoice(string choice)
+{
+
+}
+
+void afficherMessageErreur()
+{
+
+}
+
+void ajouterUnAnimal()
+{
+
+}
+
+void traiterAjoutAnimal(int i)
+{
+
+}
+
+void voirListeAnimauxPension()
+{
+
+}
+
+void voirListePropriétaire()
+{
+
+}
+
+void voirNombreTotalAnimaux()
+{
+
+}
+
+void voirPoidsTotalAnimaux()
+{
+
+}
+
+void extraireAnimauxSelonCouleurs()
+{
+
+}
+
+void retirerUnAnimalDeListe()
+{
+
+}
