ab3ae0463b50d3db614c2289c00514cfec78e64b
aro04 <mandaaroniaina2@gmail.com>
2023-11-16 10:49:14 -0500

    com

--- FrameProjet-1/FrameProjet-1/Program.cs
+++ FrameProjet-1/FrameProjet-1/Program.cs
@@ -5,21 +5,65 @@ using System.Runtime.InteropServices;
 using System.Security.Principal;
 
 string[,] tableau = new string[10, 7];
+tableau[0, 1] = "Lapin";
+tableau[1, 1] = "Chien";
+tableau[2, 1] = "Chat";
+tableau[3, 1] = "Poisson";
+tableau[4, 1] = "Lapin";
+tableau[5, 1] = "Poisson";
+
+tableau[0, 2] = "Lap";
+tableau[1, 2] = "Chio";
+tableau[2, 2] = "Chaton";
+tableau[3, 2] = "Po";
+tableau[4, 2] = "Lapinou";
+tableau[5, 2] = "Polo";
+
+tableau[0, 3] = "1";
+tableau[1, 3] = "3";
+tableau[2, 3] = "5";
+tableau[3, 3] = "12";
+tableau[4, 3] = "8";
+tableau[5, 3] = "2";
+
+tableau[0, 4] = "20";
+tableau[1, 4] = "5";
+tableau[2, 4] = "4";
+tableau[3, 4] = "15";
+tableau[4, 4] = "17";
+tableau[5, 4] = "9";
+
+tableau[0, 5] = "bleu";
+tableau[1, 5] = "rouge";
+tableau[2, 5] = "rouge";
+tableau[3, 5] = "violet";
+tableau[4, 5] = "rouge";
+tableau[5, 5] = "violet";
+
+tableau[0, 6] = "dada";
+tableau[1, 6] = "neny";
+tableau[2, 6] = "kiady";
+tableau[3, 6] = "sanda";
+tableau[4, 6] = "aro";
+tableau[5, 6] = "feno";
+
+
+
 startTheMachine();
 
 
 void afficherMenu()
 {
-    Console.WriteLine("\t ------------application de gestion de la liste des animaux en pensions--------------\n");
+   
     Console.WriteLine("1- Ajouter un animal");
-    Console.WriteLine("2- liste de tous les animaux en pension");
-    Console.WriteLine("3- liste de tous les propriétaires");
-    Console.WriteLine("4- le nombre total d’animaux en pension");
+    Console.WriteLine("2- Voir la liste de tous les animaux en pension");
+    Console.WriteLine("3- Voir liste de tous les propriétaires");
+    Console.WriteLine("4- Voir le nombre total d’animaux en pension");
     Console.WriteLine("5- Voir le poids total de tous les animaux en pension");
     Console.WriteLine("6- Voir la liste des animaux d’une couleur");
     Console.WriteLine("7- Retirer un animal");
     Console.WriteLine("8- Quiter");
-    Console.Write("\t \n Iserer votre choix: ");
+    Console.Write("\t\n ");
 
 }
 
@@ -98,44 +142,45 @@ string choisirlacouleur()
     string couleur;
     do
     {
-        Console.WriteLine("veuillez saisir la couleur de l'animal: ");
+        Console.WriteLine("Veuillez saisir la couleur de l'animal:\n ");
         couleur = Console.ReadLine();
     }
 
     while (couleur != "rouge" && couleur != "bleu" && couleur != "violet");
+    Console.WriteLine("Veuillez saisir la bonne couleur de l'animal:\n ");
     return couleur;    
 }
 
 void traiterAjoutAnimal(int i)
 {
-    Console.Write("veuillez saisir le type de l'animal: \n ");
+    Console.Write("Veuillez saisir le type de l'animal:\n ");
     string type = Console.ReadLine();
     tableau[i, 1] = type;
-    Console.Write("veuillez saisir le nom de l'animal:\n");
+    Console.Write("Veuillez saisir le nom de l'animal:\n");
     string nom = Console.ReadLine();
     tableau[i, 2] = nom;
-    Console.Write("veuillez saisir l'age de l'animal: \n");
+    Console.Write("Veuillez saisir l'age de l'animal: \n");
     string age = Console.ReadLine();
     tableau[i, 3] = age;
-    Console.Write("veuillez saisir le poids de l'animal: \n");
+    Console.Write("Veuillez saisir le poids de l'animal: \n");
     string poids = Console.ReadLine();
     tableau[i, 4] = poids;
     tableau[i, 5] = choisirlacouleur();
-    Console.Write("veuillez saisir le nom du propriétaire de l'animal: \n");
+    Console.Write("Veuillez saisir le nom du propriétaire de l'animal: \n");
     string proprietaire = Console.ReadLine();
     tableau[i, 6] = proprietaire;
 }
 void voirListeAnimauxPension()
 {
     Console.WriteLine(" --------------------------------------------------------------------------------------------------   ");
-    Console.WriteLine("ID |   TYPE   |       NOM      |       AGE      |    POIDS   |    COULEUR   |    PROPRIETAIRE    |     ");
-    Console.WriteLine(" --------------------------------------------------------------------------------------------------    ");
+    Console.WriteLine("ID |   TYPE ANIMAL   |      NOM     |   AGE  |    POIDS   |    COULEUR   |    PROPRIÉTAIRE    |     ");
+    Console.WriteLine(" ----------------------------------------------------------------------------------------------------    ");
     for (int i = 0; i < 10; i++)
     {
-        if (tableau[i, 1] != null)
+        if (tableau[i, 1] != null && tableau[i,1]!= " ")
 
         {
-            Console.WriteLine(String.Format("{0,-6}{1,-12}{2,-20}{3,-15}{4,-15}{5,-20}{6,-10}", i.ToString(), tableau[i, 1], tableau[i, 2], tableau[i, 3], tableau[i, 4], tableau[i, 5], tableau[i, 6]));
+            Console.WriteLine(String.Format("{0,-10}{1,-15}{2,-15}{3,-11}{4,-12}{5,-18}{6,-10}", i.ToString(), tableau[i, 1], tableau[i, 2], tableau[i, 3], tableau[i, 4], tableau[i, 5], tableau[i, 6]));
         }
     }
 }
@@ -153,24 +198,23 @@ void voirListePropriétaire()
 }
 
 
+
 void voirNombreTotalAnimaux()
 {
     Console.WriteLine(" ---------------------------------------------------------------------------------------");
-    Console.WriteLine("|   NOMBRE ANIMAUX |  ");
+    Console.WriteLine("|  NOMBRE ANIMAUX |  ");
     Console.WriteLine(" ---------------------------------------------------------------------------------------");
-    int NombreTotalDesAnimaux = 0;
-
-    for (int i = 0; i < 10; i++)
+    int NombreTotalAnimaux = 0; 
+    for (int i = 0; i < tableau.GetLength(0); i++)
     {
-        if (tableau[i, 1] != null)
-        {
-            NombreTotalDesAnimaux = i + 1;
-            
-        }
+        if (tableau[i,1]!=null)
+            {
+                NombreTotalAnimaux++;
+            }
+        
     }
 
-    Console.WriteLine(NombreTotalDesAnimaux);
-
+    Console.WriteLine(NombreTotalAnimaux);
 }
 
 void voirPoidsTotalAnimaux()
@@ -209,18 +253,23 @@ void extraireAnimauxSelonCouleurs()
 }
 int TrouverDernierID()
 {
-    int DernierID;
+    
     int i = 0;
     for ( i=0;i<10;i++)
     {
         if (tableau[i,1]!= null)
         {
-            DernierID = i++;
+           i = i++;
         } 
     }
     return i;
 }
-    
+
+bool EntierPositif(string ID)
+{
+    int result;
+    return Int32.TryParse(ID, out result) && result >= 0;
+}
 
 
 string entrerId()
@@ -232,26 +281,26 @@ string entrerId()
         Console.WriteLine("VEUILLEZ SAISIR LE ID DE L'ANIMAL: ");
         ID = Console.ReadLine();
     }
-    while (Int32.Parse(ID)>= DernierID);
+    while (!EntierPositif(ID) || Int32.Parse(ID)>= DernierID);
     return ID;
 }
 void retirerUnAnimalDeListe()
-{   
+{
+    int i = 0;
     string ID = entrerId();
-    for (int i = 0; i < 10; i++)
+    for ( i = 0; i < 10; i++)
     {
         if (ID == i.ToString())
         {
-
-            tableau[i, 0] = " ";
-            tableau[i, 1] = " ";
-            tableau[i, 2] = " ";
-            tableau[i, 3] = " ";
-            tableau[i, 4] = " ";
-            tableau[i, 5] = " ";
-            tableau[i, 6] = " ";
+            tableau[i, 0] = null;
+            tableau[i, 1] = null;
+            tableau[i, 2] = null;
+            tableau[i, 3] = null;
+            tableau[i, 4] = null;
+            tableau[i, 5] = null;
+            tableau[i, 6] = null;
+            
         }
-        
     }
     voirListeAnimauxPension();
 }
